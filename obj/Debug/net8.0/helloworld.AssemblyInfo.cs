3863a6200d6a0d1e9385829b29f069b2508ced88
amolnewlearning-art <amolnewlearning@gmail.com>
2025-07-30 21:36:30 +0530

    added hello world

--- obj/Debug/net8.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net8.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e5c2f5e7948c1f7ab362863027b1d17ba177b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5913fed08862206d44cab4d14625f88a5c36ebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
